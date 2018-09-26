96ba297ff91712873b2cb92e1e67955aac2487aa
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2018-09-26 23:47:01 +0800

    Test data generator.

--- webapp/server.js
+++ webapp/server.js
@@ -23,25 +23,25 @@ var java = require('java');
 java.options.push('-Dfile.encoding=UTF-8');
 var dep_base_dir = './target/classes/lib';
 fs.readdirSync(dep_base_dir).forEach(function (dep) {
-    java.classpath.push(dep_base_dir + '/' + dep);
+   java.classpath.push(dep_base_dir + '/' + dep);
 });
 java.classpath.push('target/classes');
 /*
  * Test Java Environment
  */
 var bcp = 'tech.metacontext.beancoin.common.';
-var GamePlay = java.callStaticMethodSync(bcp + 'intf.APIImpl', 'getInstance');
+var API = java.callStaticMethodSync(bcp + 'intf.APIImpl', 'getInstance');
 var Settings = java.import(bcp + 'Settings');
 var JsonObject = java.import('org.json.JSONObject');
 
 function getIncomingParams(arr) {
-    var jsonObject = new JsonObject();
-    for (var key in arr) {
-        if (arr.hasOwnProperty(key))
-            jsonObject.putSync(key, arr[key]);
-    }
-    log('* incoming data: ' + jsonObject.toStringSync());
-    return jsonObject;
+   var jsonObject = new JsonObject();
+   for (var key in arr) {
+      if (arr.hasOwnProperty(key))
+         jsonObject.putSync(key, arr[key]);
+   }
+   log('* incoming data: ' + jsonObject.toStringSync());
+   return jsonObject;
 }
 
 /*
@@ -52,11 +52,12 @@ var networkInterfaces = os.networkInterfaces();
 var request = require('supertest');
 var Transform = require('stream').Transform;
 var forever = require('forever');
+var port = 80;
 var service_url = 'http://' + (
         process.env.npm_config_serviceUrl
         || process.env.npm_package_config_serviceUrl
         || forever.config.store.serviceUrl
-        ) + ':8081/';
+        ) + ':' + port + '/';
 /*
  * Express Init
  */
@@ -66,11 +67,11 @@ var express = require('express');
 var app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
 })
         .use('/css',
                 express.static(path.join(__dirname, '/./css')))
@@ -83,193 +84,207 @@ app.use(function (req, res, next) {
 
 var commands = {
 
-    createFarmer:
-            {route: "/createFarmer", method: "POST", handler: _createFarmer},
-    getFarmer:
-            {route: "/gp/:id", method: "GET", handler: _getFarmer},
-    getMembers:
-            {route: "/getMembers", method: "GET", handler: _getMembers},
-    register:
-            {route: "/", method: "POST", handler: _register},
-    setBeanCoinRatio:
-            {route: "/bot/:id", method: "GET", handler: _setBeanCoinRatio},
-    test:
-            {route: "/test", method: "GET", handler: _test},
-    undefined:
-            {route: "/:page?", method: "GET", handler: _default},
+   createFarmer:
+           {route: "/createFarmer", method: "POST", handler: _createFarmer},
+   getFarmer:
+           {route: "/gp/:id", method: "GET", handler: _getFarmer},
+   getMembers:
+           {route: "/getMembers", method: "GET", handler: _getMembers},
+   getTransactions:
+           {route: "/getTransactions", method: "GET", handler: _getTransactions},
+   register:
+           {route: "/", method: "POST", handler: _register},
+   setBeanCoinRatio:
+           {route: "/bot/:id", method: "GET", handler: _setBeanCoinRatio},
+   test:
+           {route: "/test", method: "GET", handler: _test},
+   undefined:
+           {route: "/:page?", method: "GET", handler: _default},
 
-    handler: (req, res, key) => {
-        log('* incoming request: ' + key);
-        commands[key].handler(req, res);
-    }
+   handler: (req, res, key) => {
+      log('* incoming request: ' + key);
+      commands[key].handler(req, res);
+   }
 };
 
 Object.keys(commands).forEach((key) => {
-    switch (commands[key].method) {
-        case "ALL":
-            app.all(commands[key].route, (req, res) => commands.handler(req, res, key));
-            break;
-        case "POST":
-            app.post(commands[key].route, (req, res) => commands.handler(req, res, key));
-            break;
-        case "GET":
-            app.get(commands[key].route, (req, res) => commands.handler(req, res, key));
-            break;
-    }
+   switch (commands[key].method) {
+      case "ALL":
+         app.all(commands[key].route, (req, res) => commands.handler(req, res, key));
+         break;
+      case "POST":
+         app.post(commands[key].route, (req, res) => commands.handler(req, res, key));
+         break;
+      case "GET":
+         app.get(commands[key].route, (req, res) => commands.handler(req, res, key));
+         break;
+   }
 });
 
 
 
 function _createFarmer(req, res) {
-    var params = getIncomingParams(req.body);
-    var retVal = JSON.parse(GamePlay.createSessionSync(params));
-    res.send(retVal);
+   var params = getIncomingParams(req.body);
+   var retVal = JSON.parse(API.createSessionSync(params));
+   res.send(retVal);
 }
 
 function _getFarmer(req, res) {
-    var id = req.params.id;
-    var params = getIncomingParams(req.query).putSync('id', id);
-    var retVal = JSON.parse(GamePlay.gamePlaySync(params));
-    if (retVal.error) {
-        res.send(retVal);
-    } else {
-        var session_id = retVal.session_id;
-        var bot_id = retVal.bot_id;
-        var title = retVal.title;
-        var description = retVal.description;
-        var hashcode = retVal.hashcode;
-        var questionaire = 'https://docs.google.com/forms/d/e/1FAIpQLSfJp_2eClROlSundBdQHCbcA3ykSmT590D-hgBQgIJDmO8Hcw/viewform?entry.873951504=';
+   var id = req.params.id;
+   var params = getIncomingParams(req.query).putSync('id', id);
+   var retVal = JSON.parse(API.gamePlaySync(params));
+   if (retVal.error) {
+      res.send(retVal);
+   } else {
+      var session_id = retVal.session_id;
+      var bot_id = retVal.bot_id;
+      var title = retVal.title;
+      var description = retVal.description;
+      var hashcode = retVal.hashcode;
+      var questionaire = 'https://docs.google.com/forms/d/e/1FAIpQLSfJp_2eClROlSundBdQHCbcA3ykSmT590D-hgBQgIJDmO8Hcw/viewform?entry.873951504=';
 //                      'https://goo.gl/forms/ykI3IPfCiGJABeQg2'
-        res.write('<!-- Begin stream -->\n');
-        fs.createReadStream(path.join(__dirname, '/index.html'))
-                .pipe(getParser(session_id, bot_id, title, description, hashcode, questionaire))
-                .on('end', () => res.write('\n<!-- End stream -->'))
-                .pipe(res);
-    }
+      res.write('<!-- Begin stream -->\n');
+      fs.createReadStream(path.join(__dirname, '/index.html'))
+              .pipe(getParser(session_id, bot_id, title, description, hashcode, questionaire))
+              .on('end', () => res.write('\n<!-- End stream -->'))
+              .pipe(res);
+   }
 
-    function getParser(session_id, bot_id, title, description, hashcode, questionaire) {
-        var parser = new Transform();
-        parser._transform = function (data, encoding, done) {
-            const str = data.toString()
-                    .replace('{{service_url}}', service_url)
-                    .replace('{{session_id}}', session_id)
-                    .replace('{{bot_id}}', bot_id)
-                    .replace('{{title}}', title)
-                    .replace('{{description}}', description)
-                    .replace('{{hashcode}}', hashcode)
-                    .replace('{{questionaire}}', questionaire);
-            this.push(str);
-            done();
-        };
-        return parser;
-    }
+   function getParser(session_id, bot_id, title, description, hashcode, questionaire) {
+      var parser = new Transform();
+      parser._transform = function (data, encoding, done) {
+         const str = data.toString()
+                 .replace('{{service_url}}', service_url)
+                 .replace('{{session_id}}', session_id)
+                 .replace('{{bot_id}}', bot_id)
+                 .replace('{{title}}', title)
+                 .replace('{{description}}', description)
+                 .replace('{{hashcode}}', hashcode)
+                 .replace('{{questionaire}}', questionaire);
+         this.push(str);
+         done();
+      };
+      return parser;
+   }
 }
 
 function _register(req, res) {
-    var bot_list = JSON.parse(GamePlay.listAvailableBotsSync());
-    res.send(bot_list);
+   var bot_list = JSON.parse(API.listAvailableBotsSync());
+   res.send(bot_list);
 }
 
 function _setBeanCoinRatio(req, res) {
-    var id = req.params.id;
-    var bot_session = JSON.parse(GamePlay.createSessionSync(id, Settings.Mode.PUBLISH));
-    res.send(bot_session);
+   var id = req.params.id;
+   var bot_session = JSON.parse(API.createSessionSync(id, Settings.Mode.PUBLISH));
+   res.send(bot_session);
 }
 
 function _getMembers(req, res) {
-    var reaction = new JsonObject();
-    if (req.params.article) {
-        reaction.putSync('selected', req.params.article);
-    }
-    if (req.body) {
-        reaction.putSync('input', req.body.input);
-    }
-    var next_node = JSON.parse(GamePlay.getNextNodeSync(req.params.session, reaction));
-    log(JSON.stringify(next_node));
-    var ScenicNode = next_node.ScenicNode;
-    if (ScenicNode && ScenicNode.node_type === "NodeLinker_Random") {
-        next_node = JSON.parse(GamePlay.getNextNodeSync(req.params.session, reaction));
-        log(JSON.stringify(next_node));
-    }
-    res.send(next_node);
+   var params = new JsonObject();
+   var result = JSON.parse(API.getMembersSync(params));
+   log(JSON.stringify(result));
+   res.send(result);
+}
+
+function _getTransactions(req, res) {
+   var params = new JsonObject();
+   var result = JSON.parse(API.getTransactionsSync(params));
+   log(JSON.stringify(result));
+   res.send(result);
 }
 
 function _test(req, res) {
-    var identity = Settings.test_identity;
-    var params = {
-        stage: 'begin',
-        identity: identity,
-        request_id: 'hello'};
-    log('negotiate: begin');
-    request(app).post('/negotiate')
-            .send(params)
-            .end(_createSession);
-    function _createSession(err, res0) {
-        log('createSession');
-        var body = res0.body;
-        log(body);
-        var payload = body.payload;
-        var kptool = java.newInstanceSync('io.xtrea.common.crypto.KeyPairTool');
-        var keyPairId = java.callStaticMethodSync(
-                'io.xtrea.bot.ilya_bot.interfaces.Tools',
-                'checkIdentity', kptool, identity);
-        kptool.useKeyPairSync(keyPairId);
-        var params = {
-            identity: identity,
-            request_id: body.request_id,
-            session_id: kptool.decryptSync(payload).getMessageSync(),
-//               game_id: 'DEFOCUSED_EYES'
-//         game_id: 'MARS_IMMIGRANT'
-            game_id: 'DEMO_SCRIPT'
-        };
-        request(app).post('/createSession')
-                .send(params)
-                .end(_gamePlay);
-    }
-
-    function _gamePlay(err, res0) {
-        log('gamePlay');
-        var body = res0.body;
-        log(body);
-        request(app).get('/' + body.entry)
-                .end((err, res0) => res.send(res0.text));
-    }
+   var params = new JsonObject();
+   var size = 10.0;
+   for (var j = 0; j < 5; j++) {
+      var farmer = JSON.parse(API.createFarmerSync(params.putSync("size", size)));
+      log(JSON.stringify(farmer));
+      params.putSync("id", farmer.id);
+      for (var i = 0; i < 10; i++) {
+         API.manageMaterialSync(params.putSync("material_id", 0).putSync("amount", size));
+         API.manageMaterialSync(params.putSync("material_id", 1).putSync("amount", size));
+         API.manageMaterialSync(params.putSync("material_id", 2).putSync("amount", size));
+         var crop = JSON.parse(API.produceCropSync(params));
+         log(JSON.stringify(crop));
+      }
+   }
+   res.send("Test Data Generated.");
 }
-
+/*
+ var params = {
+ stage: 'begin',
+ identity: identity,
+ request_id: 'hello'};
+ log('negotiate: begin');
+ request(app).post('/negotiate')
+ .send(params)
+ .end(_createSession);
+ function _createSession(err, res0) {
+ log('createSession');
+ var body = res0.body;
+ log(body);
+ var payload = body.payload;
+ var kptool = java.newInstanceSync('io.xtrea.common.crypto.KeyPairTool');
+ var keyPairId = java.callStaticMethodSync(
+ 'io.xtrea.bot.ilya_bot.interfaces.Tools',
+ 'checkIdentity', kptool, identity);
+ kptool.useKeyPairSync(keyPairId);
+ var params = {
+ identity: identity,
+ request_id: body.request_id,
+ session_id: kptool.decryptSync(payload).getMessageSync(),
+ //               game_id: 'DEFOCUSED_EYES'
+ //         game_id: 'MARS_IMMIGRANT'
+ game_id: 'DEMO_SCRIPT'
+ };
+ request(app).post('/createSession')
+ .send(params)
+ .end(_gamePlay);
+ }
+ 
+ function _gamePlay(err, res0) {
+ log('gamePlay');
+ var body = res0.body;
+ log(body);
+ request(app).get('/' + body.entry)
+ .end((err, res0) => res.send(res0.text));
+ }
+ }
+ */
 function _default(req, res) {
-    var page = req.params.page;
-    log('page = ' + req.params.page);
-    switch (page) {
-        case 'favicon.ico':
-            res.sendFile(path.join(__dirname, '/images/favicon.ico'));
-            break;
-        default:
-            res.status(404).send('Page not found');
-    }
+   var page = req.params.page;
+   log('page = ' + req.params.page);
+   switch (page) {
+      case 'favicon.ico':
+         res.sendFile(path.join(__dirname, '/images/favicon.ico'));
+         break;
+      default:
+         res.status(404).send('Page not found');
+   }
 }
 
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Listening to ' + host + ':' + port);
-    console.log('service_url = ' + service_url);
+var server = app.listen(port, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log('Listening to ' + host + ':' + port);
+   console.log('service_url = ' + service_url);
 });
 
 function log(obj) {
-    var date = new Date();
-    var hour = date.getHours();
-    hour = (hour < 10 ? '0' : '') + hour;
-    var min = date.getMinutes();
-    min = (min < 10 ? '0' : '') + min;
-    var sec = date.getSeconds();
-    sec = (sec < 10 ? '0' : '') + sec;
-    var mil_sec = date.getMilliseconds();
-    mil_sec = (mil_sec < 100 ? '0' : '') + (mil_sec < 10 ? '0' : '') + mil_sec;
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? '0' : '') + month;
-    var day = date.getDate();
-    day = (day < 10 ? '0' : '') + day;
-    var day_time = year + '/' + month + '/' + day + ' ' + hour + ':' + min + ':' + sec + '.' + mil_sec;
-    console.log((obj + '' === obj) ? '\n%s %s' : '\n%s %O', day_time, obj);
+   var date = new Date();
+   var hour = date.getHours();
+   hour = (hour < 10 ? '0' : '') + hour;
+   var min = date.getMinutes();
+   min = (min < 10 ? '0' : '') + min;
+   var sec = date.getSeconds();
+   sec = (sec < 10 ? '0' : '') + sec;
+   var mil_sec = date.getMilliseconds();
+   mil_sec = (mil_sec < 100 ? '0' : '') + (mil_sec < 10 ? '0' : '') + mil_sec;
+   var year = date.getFullYear();
+   var month = date.getMonth() + 1;
+   month = (month < 10 ? '0' : '') + month;
+   var day = date.getDate();
+   day = (day < 10 ? '0' : '') + day;
+   var day_time = year + '/' + month + '/' + day + ' ' + hour + ':' + min + ':' + sec + '.' + mil_sec;
+   console.log((obj + '' === obj) ? '\n%s %s' : '\n%s %O', day_time, obj);
 }
